e34a8ad0feca533e612879c30a09ed640164ee94
rahull0328 <rmmehta2004@gmail.com>
2025-05-18 01:52:43 +0530

    completed UI for updateProfile Dialog

---
+++ frontend/src/components/profile/UpdateProfile.jsx
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Loader2 } from "lucide-react";
+
+const UpdateProfile = ({ open, setOpen }) => {
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <div>
+      <Dialog open={open}>
+        <DialogContent
+          className="sm:max-w-[425px]"
+          onInteractOutside={() => setOpen(false)}
+        >
+          <DialogHeader>
+            <DialogTitle>Update Profile</DialogTitle>
+          </DialogHeader>
+          <form>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input
+                  id="name"
+                  name="name"
+                  type="text"
+                  // value={input.fullname}
+                  // onChange={changeEventHandler}
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="email" className="text-right">
+                  Email
+                </Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  // value={input.email}
+                  // onChange={changeEventHandler}
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="number" className="text-right">
+                  Number
+                </Label>
+                <Input
+                  id="number"
+                  name="number"
+                  // value={input.phoneNumber}
+                  // onChange={changeEventHandler}
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="bio" className="text-right">
+                  Bio
+                </Label>
+                <Input
+                  id="bio"
+                  name="bio"
+                  // value={input.bio}
+                  // onChange={changeEventHandler}
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="skills" className="text-right">
+                  Skills
+                </Label>
+                <Input
+                  id="skills"
+                  name="skills"
+                  // value={input.skills}
+                  // onChange={changeEventHandler}
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="file" className="text-right">
+                  Resume
+                </Label>
+                <Input
+                  id="file"
+                  name="file"
+                  type="file"
+                  accept="application/pdf"
+                  // onChange={fileChangeHandler}
+                  className="col-span-3"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              {loading ? (
+                <Button className="w-full my-4">
+                  {" "}
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait{" "}
+                </Button>
+              ) : (
+                <Button type="submit" className="w-full my-4">
+                  Update
+                </Button>
+              )}
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default UpdateProfile;
