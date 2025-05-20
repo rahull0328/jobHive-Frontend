64b153f93696bd7d8b4d4f1ae0fb7cfbb68e5bef
rahull0328 <rmmehta2004@gmail.com>
2025-05-21 01:34:39 +0530

    completed apply to job api integration with UI

--- frontend/src/components/jobs/JobDescription.jsx
+++ frontend/src/components/jobs/JobDescription.jsx
@@ -1,58 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { Badge } from "../ui/badge";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-
 import { setSingleJob } from "@/redux/jobSlice";
-import store from "@/redux/store";
 import Navbar from "../shared/Navbar";
 import { toast } from "sonner";
 import { APPLICATION_API_END_POINT, JOB_API_END_POINT } from "@/utils/constant";
+import store from "@/redux/store";
 
 const JobDescription = () => {
-  const { singleJob } = useSelector((store) => store.job);
-  const { user } = useSelector((store) => store.auth);
-  const isApplied = singleJob?.applications?.some(
-    (application) => application.applicant === user?._id || false
-  );
 
-  const params = useParams();
-  const jobId = params.id;
-  const dispatch = useDispatch();
+  const {singleJob} = useSelector(store=>store.job)
+  const {user} = useSelector(store=>store.auth)
+  const isInitiallyApplied = singleJob?.applications?.some(application=>application.applicant === user?._id) || false
+  const [isApplied, setIsApplied] = useState(isInitiallyApplied)
 
-  const applyJobHandler = async (e) => {
+  const params = useParams()
+  const jobId = params.id
+  const dispatch = useDispatch()
+
+  const applyJobHandler = async () => {
     try {
-      const res = await axios.post(
-        `${APPLICATION_API_END_POINT}/apply/${jobId}`,
-        {},
-        { withCredentials: true }
-      );
+      const res = await axios.post(`${APPLICATION_API_END_POINT}/apply/${jobId}`, {}, {withCredentials: true})
       if (res.data.success) {
-        toast.success(res.data.message);
+        setIsApplied(true)
+        const updatedSingleJob = {...singleJob, applications: [...singleJob.applications, {applicant: user?._id}]}
+        dispatch(setSingleJob(updatedSingleJob))
+        toast.success(res.data.message)
       }
     } catch (error) {
-      console.log(error);
-      toast.error(error.response.data.message);
+      console.log(error)
+      toast.error(error.response.data.message)
     }
-  };
+  }
 
   useEffect(() => {
     const fetchSingleJob = async () => {
       try {
-        const res = await axios.get(`${JOB_API_END_POINT}/get/${jobId}`, {
-          withCredentials: true,
-        });
-        if (res.data.success) {
-          dispatch(setSingleJob(res.data.job));
+        const res = await axios.get(`${JOB_API_END_POINT}/get/${jobId}`, {withCredentials: true})
+        if(res.data.success) {
+          dispatch(setSingleJob(res.data.job))
+          setIsApplied(res.data.job.application.some(application=>application.applicant === user?._id))
         }
       } catch (error) {
         console.log(error);
       }
-    };
-    fetchSingleJob();
-  }, [jobId, dispatch, user?._id]);
+    }
+    fetchSingleJob(); 
+  }, [jobId, dispatch, user?._id])
 
   return (
     <div>
@@ -63,7 +60,7 @@ const JobDescription = () => {
             <h1 className="font-bold text-xl">{singleJob?.title}</h1>
             <div className="flex items-center gap-2 mt-4">
               <Badge className={"text-blue-700 font-bold"} variant="ghost">
-                {singleJob?.position} Postions
+                {singleJob?.position} Positions
               </Badge>
               <Badge className={"text-[#F83002] font-bold"} variant="ghost">
                 {singleJob?.jobType}
@@ -85,6 +82,7 @@ const JobDescription = () => {
             {isApplied ? "Already Applied" : "Apply Now"}
           </Button>
         </div>
+
         <h1 className="border-b-2 border-b-gray-300 font-medium py-4">
           Job Description
         </h1>
@@ -128,7 +126,7 @@ const JobDescription = () => {
           <h1 className="font-bold my-1">
             Posted Date:
             <span className="pl-4 font-normal text-gray-800">
-              {singleJob?.createdAt.split("T")[0]}
+              {new Date(singleJob?.createdAt).toLocaleDateString()}
             </span>
           </h1>
         </div>
