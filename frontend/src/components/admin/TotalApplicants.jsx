ed76f427b602bc570fac25011f9b3a42bc6cdddf
rahull0328 <rmmehta2004@gmail.com>
2025-05-25 17:54:47 +0530

    added hook for useGetAllAppliedJobs

--- frontend/src/components/admin/TotalApplicants.jsx
+++ frontend/src/components/admin/TotalApplicants.jsx
@@ -1,48 +1,48 @@
-import React, { useEffect } from "react";
-import Navbar from "../shared/Navbar";
-import { useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import store from "@/redux/store";
-import ApplicantsInfo from "./ApplicantsInfo";
-import axios from "axios";
-import { APPLICATION_API_END_POINT } from "@/utils/constant";
-import { setAllApplicants } from "@/redux/applicationSlice";
+  import React, { useEffect } from "react";
+  import Navbar from "../shared/Navbar";
+  import { useParams } from "react-router-dom";
+  import { useDispatch, useSelector } from "react-redux";
+  import store from "@/redux/store";
+  import ApplicantsInfo from "./ApplicantsInfo";
+  import axios from "axios";
+  import { APPLICATION_API_END_POINT } from "@/utils/constant";
+  import { setAllApplicants } from "@/redux/applicationSlice";
 
-const TotalApplicants = () => {
-  const params = useParams();
-  const dispatch = useDispatch();
-  const { applicants } = useSelector((store) => store.application);
+  const TotalApplicants = () => {
+    const params = useParams();
+    const dispatch = useDispatch();
+    const { applicants } = useSelector((store) => store.application);
 
-  useEffect(() => {
-    const fetchApplicants = async () => {
-      try {
-        const res = await axios.get(
-          `${APPLICATION_API_END_POINT}/${params.id}/applicants`,
-          { withCredentials: true }
-        );
-        if (res.data.success) {
-          dispatch(setAllApplicants(res.data.job));
+    useEffect(() => {
+      const fetchApplicants = async () => {
+        try {
+          const res = await axios.get(
+            `${APPLICATION_API_END_POINT}/${params.id}/applicants`,
+            { withCredentials: true }
+          );
+          if (res.data.success) {
+            dispatch(setAllApplicants(res.data.job));
+          }
+        } catch (error) {
+          console.log(error);
         }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchApplicants();
-  }, []);
+      };
+      fetchApplicants();
+    }, []);
 
-  return (
-    <div className="min-h-screen bg-gray-50 mt-10">
-      <Navbar />
-      <div className="w-full flex justify-center px-4 py-8">
-        <div className="w-full max-w-6xl">
-          <h1 className="font-bold text-2xl mb-6">
-            Applicants ({applicants?.applications?.length || 0})
-          </h1>
-          <ApplicantsInfo />
+    return (
+      <div className="min-h-screen bg-gray-50 mt-16">
+        <Navbar />
+        <div className="w-full flex justify-center px-4 py-8">
+          <div className="w-full max-w-6xl">
+            <h1 className="font-bold text-2xl mb-6">
+              Applicants ({applicants?.applications?.length || 0})
+            </h1>
+            <ApplicantsInfo />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default TotalApplicants;
+  export default TotalApplicants;
--- frontend/src/redux/jobSlice.js
+++ frontend/src/redux/jobSlice.js
@@ -1,30 +1,40 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const jobSlice = createSlice({
-    name: "job",
-    initialState: {
-        allJobs: [],
-        singleJob: null,
-        allAdminJobs: [],
-        loading: false,
-        searchJobByText: "",
-        getJobById: null,
+  name: "job",
+  initialState: {
+    allJobs: [],
+    singleJob: null,
+    allAdminJobs: [],
+    loading: false,
+    searchJobByText: "",
+    getJobById: null,
+    allAppliedJobs: [],
+  },
+  reducers: {
+    setAllJobs: (state, action) => {
+      state.allJobs = action.payload;
     },
-    reducers: {
-        setAllJobs: (state, action) => {
-            state.allJobs = action.payload;
-        },
-        setSingleJob: (state, action) => {
-            state.singleJob = action.payload;
-        },
-        setAllAdminJobs: (state, action) => {
-            state.allAdminJobs = action.payload
-        },
-        setSearchJobByText: (state, action) => {
-            state.searchJobByText = action.payload
-        },
-     }
+    setSingleJob: (state, action) => {
+      state.singleJob = action.payload;
+    },
+    setAllAdminJobs: (state, action) => {
+      state.allAdminJobs = action.payload;
+    },
+    setSearchJobByText: (state, action) => {
+      state.searchJobByText = action.payload;
+    },
+    setAllAppliedJobs: (state, action) => {
+      state.allAppliedJobs = action.payload;
+    },
+  },
 });
 
-export const { setAllJobs, setSingleJob, setAllAdminJobs, setSearchJobByText } = jobSlice.actions;
+export const {
+  setAllJobs,
+  setSingleJob,
+  setAllAdminJobs,
+  setSearchJobByText,
+  setAllAppliedJobs,
+} = jobSlice.actions;
 export default jobSlice.reducer;
