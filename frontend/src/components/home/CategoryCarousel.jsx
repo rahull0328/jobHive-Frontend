500912823285b460004b9e34a25e1ef234a3b577
rahull0328 <rmmehta2004@gmail.com>
2025-05-27 13:22:15 +0530

    started workion Job Search functionality

--- frontend/src/components/home/CategoryCarousel.jsx
+++ frontend/src/components/home/CategoryCarousel.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { 
   Carousel, 
   CarouselContent, 
@@ -8,6 +8,9 @@ import {
 } from '../ui/carousel'
 import { Button } from '../ui/button'
 import { Monitor, Server, Code2, BarChart, Settings2 } from 'lucide-react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { setSearchedQuery } from '@/redux/jobSlice'
 
 const category = [
   { label: "Frontend Developer", icon: <Monitor className="w-4 h-4 mr-2" /> },
@@ -18,6 +21,15 @@ const category = [
 ]
 
 const CategoryCarousel = () => {
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const searchJobHandler = (query) => {
+    dispatch(setSearchedQuery(query))   
+    navigate("/browse")
+  }
+
   return (
     <div className="">
       <h4 className="text-2xl font-bold text-center">Explore Categories</h4>
@@ -29,7 +41,8 @@ const CategoryCarousel = () => {
                 key={index} 
                 className="md:basis-1/2 lg:basis-1/3"
               >
-                <Button 
+                <Button
+                  onClick={() => searchJobHandler(cat)}
                   variant="outline" 
                   className="rounded-full w-full py-4 text-sm font-medium transition-all hover:bg-primary hover:text-white"
                 >
--- frontend/src/components/home/HeroSection.jsx
+++ frontend/src/components/home/HeroSection.jsx
@@ -1,8 +1,21 @@
 import { Search } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../ui/button";
+import { useDispatch } from "react-redux";
+import { setSearchedQuery } from "@/redux/jobSlice";
+import { useNavigate } from "react-router-dom";
 
 const HeroSection = () => {
+
+  const [query, setQuery] = useState("")
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const searchJobHandler = () => {
+    dispatch(setSearchedQuery(query))   
+    navigate("/browse")
+  }
+
   return (
     <div className="text-center">
       <div className="flex flex-col gap-5 my-26">
@@ -20,10 +33,14 @@ const HeroSection = () => {
         <div className="flex w-[40%] shadow-lg border mt-6 border-gray-200 pl-3 h-11 rounded-full items-center gap-4 mx-auto">
           <input
             type="text"
+            onChange={(e) => setQuery(e.target.value)}
             placeholder="🔍 Search jobs by title"
             className="outline-none border-none w-full"
           />
-          <Button className="rounded-r-full h-11 bg-[#EF88AD]">
+          <Button 
+            onClick={searchJobHandler}
+            className="rounded-r-full h-11 bg-[#EF88AD]"
+          >
             <Search className="h-6 w-6" />
           </Button>
         </div>
