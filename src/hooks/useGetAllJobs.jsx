e42c196ef792261ad925d6cb01666055aa8e3a33
rahull0328 <rmmehta2004@gmail.com>
2025-08-16 13:54:22 +0530

    Inital commit

--- src/hooks/useGetAllJobs.jsx
+++ src/hooks/useGetAllJobs.jsx
@@ -3,7 +3,6 @@ import { JOB_API_END_POINT } from "@/utils/constant";
 import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
 const useGetAllJobs = () => {
   const dispatch = useDispatch();
   const { searchedQuery } = useSelector((store) => store.job);
